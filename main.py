0dd9b27e059a56674a28e2269150a3221cef554b
Cesar Zapata <cesarzr10@gmail.com>
2022-06-16 11:41:09 -0500

    Creating val, train, and defining jaccard as the first way to evaluate

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from cgitb import grey
 import os
 import glob
 import numpy as np
 import skimage.io as io
 from skimage.color import rgb2gray
 from matplotlib import pyplot as plt
-from skimage.filters import threshold_otsu
-
+from sklearn.metrics import jaccard_score
 
 # Cargando las imagenes en un dataframe
 rect_df = glob.glob(os.path.join('data/', 'rect_*.jpg'))
 circ_df = glob.glob(os.path.join('data/', 'circ_*.jpg'))
+validation_df = glob.glob(os.path.join('val/', '*.jpg'))
+training_df = glob.glob(os.path.join('train/', '*.jpg'))
 
 # Function for the preprocessing of the images
 def preprocessing(df):
@@ -25,6 +25,14 @@ def preprocessing(df):
 
 rects = preprocessing(rect_df) # array for rectangle images
 circles = preprocessing(circ_df) # array for circle images
+validation = preprocessing(validation_df) # array for validation images
+training = preprocessing(training_df)
 
-plt.imshow(circles[3], cmap='gray')
-plt.show()
+ground_truth_train = [1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0] # where 1=circle, 0=rectangle
+ground_truth_val = [1, 1, 0, 1, 0, 0] 
+
+
+base = np.zeros((20, 20)) # creating blank canvas 
+
+
+# utilizar indice de jaccard para ver precision y cobertura del algoritmo
